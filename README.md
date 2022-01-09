6ddec132bed665a87d32355b4b59fb0b449d08fb
ambar gupta <ambar.gtm@gmail.com>
2022-01-09 14:41:47 +0530

    Implementing oAuth security to inMemory Token store feature

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # udemy_springSecurity_coupon_service
-this is coupon service for generate and get coupon code with have cusotom Spring security 
+this is coupon service for generate and get coupon code with have custom Spring security 
+here we are implementing  Spring Oauth2.0 using the Spring old version 2.2.6 before 2.4 Spring Authorization server 
+is part of Spring cloud but inl latest it developed by Spring as separate project as its production ha been release for use in 19 Aug 2022  we  will use it in letter 
---
+++ src/main/java/com/learningpath/couponsservice/security/configuration/OAuth2SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.learningpath.couponsservice.security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class OAuth2SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+     public AuthenticationManager authenticationManagerBean() throws Exception {
+         return super.authenticationManagerBean();
+     }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder()
+    {
+        return  new BCryptPasswordEncoder();
+    }
+}
