70ee3673684b34aefffa1ea5eb9a812af571dd1c
ambar gupta <ambar.gtm@gmail.com>
2022-01-13 08:51:24 +0530

    OAuth implementation with JWT

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # udemy_springSecurity_coupon_service
 this is coupon service for generate and get coupon code with have custom Spring security 
 here we are implementing  Spring Oauth2.0 using the Spring old version 2.2.6 before 2.4 Spring Authorization server 
-is part of Spring cloud but inl latest it developed by Spring as separate project as its production ha been release for use in 19 Aug 2022  we  will use it in letter 
+is part of Spring cloud but inl latest it developed by Spring as separate project as its production ha been release for use in 19 Aug 2022 we  will use it in letter
+
+Here we are Implementing the JWT with Oauth Spring Security 
+in this implementation we have used Authorization server & Resource server in same project
+in next part we remove the Authorization server  from  coupon service and create a separate one .
+resource server still will be part of coupon service 
--- src/main/java/com/learningpath/couponsservice/security/configuration/AuthorizationServerConfig.java
+++ src/main/java/com/learningpath/couponsservice/security/configuration/AuthorizationServerConfig.java
@@ -2,18 +2,23 @@ package com.learningpath.couponsservice.security.configuration;
 
 import com.learningpath.couponsservice.security.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import javax.sql.DataSource;
+import java.security.KeyPair;
 
 @Configuration
 @EnableAuthorizationServer
@@ -30,9 +35,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private DataSource dataSource;
 
+    @Value("${resource}")
+    private String resource;
+    @Value("${password}")
+    private String password;
+    @Value("${alias}")
+    private String alias;
+
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.tokenStore(new JdbcTokenStore(dataSource)).
+        endpoints.tokenStore(tokenStore()).accessTokenConverter(jwtAccessTokenConverter()).
                 authenticationManager(authenticationManager).userDetailsService(userDetailsService);
     }
 
@@ -41,5 +53,21 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         clients.inMemory().withClient("couponclientapp").secret(passwordEncoder.encode("9999"))
                 .authorizedGrantTypes("password","refresh_token").scopes("read","write").resourceIds(RESOURCE_ID);
     }
+    @Bean
+    public TokenStore tokenStore()
+    {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+     public JwtAccessTokenConverter jwtAccessTokenConverter()
+     {
+         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+         KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource(resource),password.toCharArray());
+         KeyPair keyPair = keyStoreKeyFactory.getKeyPair(alias);
+         jwtAccessTokenConverter.setKeyPair(keyPair);
+         return jwtAccessTokenConverter;
+
+     }
 
 }
